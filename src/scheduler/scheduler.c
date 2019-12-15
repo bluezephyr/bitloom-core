7b82fbb73b931e68dfaef36c56dc048731415d02
Blue Zephyr <blue.zephyr.git@gmail.com>
2019-12-15 21:30:38 +0100

    Replace all module variables with self variable

--- src/scheduler/scheduler.c
+++ src/scheduler/scheduler.c
@@ -1,7 +1,7 @@
 /*
  * BitLoom Scheduler - Simple non-preemptive scheduler for up to 32 tasks.
  *
- * Copyright (c) 2016-2018. BlueZephyr
+ * Copyright (c) 2016-2019. BlueZephyr
  *
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
@@ -18,39 +18,42 @@ typedef struct Task_t
     task_run run;   // The task's run function
 } Task_t;
 
-static volatile Task_t tasks[SCHEDULER_NO_TASKS];
-static volatile uint8_t added_tasks = 0;
-
 /*
  * Corresponding bit in the overrun flag is set if a task exceeds the maximum
  * execution time.  The task_error variable holds the id:s of all tasks that
  * have overrun.
  */
-static volatile uint32_t overrun;
-static volatile uint32_t task_error;
+typedef struct Scheduler_t
+{
+   Task_t tasks[SCHEDULER_NO_TASKS];
+   uint8_t no_of_tasks;
+   uint32_t overrun;
+   uint32_t task_error;
+} Scheduler_t;
 
+static volatile Scheduler_t self;
 
 void schedule_init (void)
 {
-    overrun = 0;
-    task_error = 0;
-    added_tasks = 0;
+    self.overrun = 0;
+    self.task_error = 0;
+    self.no_of_tasks = 0;
 }
 
 uint32_t schedule_get_overrun_tasks(void)
 {
-    return task_error;
+    return self.task_error;
 }
 
 uint8_t schedule_add_task (uint8_t period, uint8_t offset,
         task_run run_function)
 {
-    if(added_tasks < SCHEDULER_NO_TASKS)
+    if(self.no_of_tasks < SCHEDULER_NO_TASKS)
     {
-        tasks[added_tasks].period = period;
-        tasks[added_tasks].time = period + offset;
-        tasks[added_tasks].run = run_function;
-        return added_tasks++;
+        self.tasks[self.no_of_tasks].period = period;
+        self.tasks[self.no_of_tasks].time = period + offset;
+        self.tasks[self.no_of_tasks].run = run_function;
+        return self.no_of_tasks++;
     }
     else
     {
@@ -73,18 +76,18 @@ void schedule_timer_tick (void)
 {
     uint8_t i;
 
-    if (overrun)
+    if (self.overrun)
     {
         // One of the tasks exceeded its execution time
-        task_error = overrun;
+        self.task_error = self.overrun;
     }
 
     // Decrease the current time value for all tasks
-    for (i=0; i<added_tasks; i++)
+    for (i=0; i<self.no_of_tasks; i++)
     {
-        if(tasks[i].time > 0)
+        if(self.tasks[i].time > 0)
         {
-            tasks[i].time--;
+            self.tasks[i].time--;
         }
     }
 }
@@ -93,14 +96,14 @@ void schedule_run (void)
 {
     uint8_t task;
 
-    for (task=0; task<added_tasks; task++)
+    for (task=0; task<self.no_of_tasks; task++)
     {
-        if (tasks[task].time == 0)
+        if (self.tasks[task].time == 0)
         {
-            overrun |= (1u << task);
-            tasks[task].time = tasks[task].period;
-            tasks[task].run();
-            overrun &= ~(1u << task);
+            self.overrun |= (1u << task);
+            self.tasks[task].time = self.tasks[task].period;
+            self.tasks[task].run();
+            self.overrun &= ~(1u << task);
         }
     }
 }
